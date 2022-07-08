1128503112f938754be7d3aa95f629df5defdc36
neihcogn49520 <ngochien04092001@gmail.com>
2022-07-08 13:16:54 +0700

    add posts.js file in routes and controllers

--- index.js
+++ index.js
@@ -2,20 +2,21 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import { error } from 'console';
+
+import postRoutes from './routes/posts.js';
 
 const app = express();
+
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 const CONNECTION_URL =
-    "mongodb+srv://neihcogn49520:@Ngochien495201@cluster0.qyije.mongodb.net/?retryWrites=true&w=majority";
+    "mongodb+srv://neihcogn49520:%40Ngochien495201@cluster0.qyije.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL)
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-    .catch((console.error(error.message)));
-
-// mongoose.set('useFindAndModify', false);
+    .catch((error) => console.log(error.message));
